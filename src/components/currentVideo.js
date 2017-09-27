233e2d262c40f1c782c96464ab812c820c9bab17
bnoden <brandroid369@gmail.com>
2017-09-27 00:05:11 -0500

    include media queries for video size

--- src/components/currentVideo.js
+++ src/components/currentVideo.js
@@ -4,14 +4,22 @@ import video1 from './video/dits01_480.mp4';
 
 class CurrentVideo extends Component {
   render() {
-
     return (
       <div className="currentVideo">
-        <video ref="curvid" className="vid" width="720" height="auto" onClick={()=>{this.refs.curvid.paused?this.refs.curvid.play():this.refs.curvid.pause()}} controls>
+        <video
+          ref="curvid"
+          className="vid"
+          onClick={() => {
+            this.refs.curvid.paused
+              ? this.refs.curvid.play()
+              : this.refs.curvid.pause();
+          }}
+          controls
+        >
           <source src={video1} type="video/mp4" />
         </video>
       </div>
-    )
+    );
   }
 }
 
