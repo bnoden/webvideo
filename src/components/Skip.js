1430ef20c88e0d20dd41e34de60239bf3a95b9d0
bnoden <brandroid369@gmail.com>
2017-10-02 01:50:51 -0500

    premature optimization

--- src/components/Skip.js
+++ src/components/Skip.js
@@ -4,34 +4,59 @@ import { video } from './CurrentVideo';
 import './styles/Skip.css';
 
 class Skip extends Component {
-
   render() {
-    const ffInput = () => document.getElementById('input-skip-ff').value;
+    const ffInput = () => document.getElementById('input-skip-ff');
+    const rwInput = () => document.getElementById('input-skip-rw');
     return (
       <div className="skip">
+        <input
+          type="number"
+          id="input-skip-rw"
+          min="-60"
+          max="0"
+          defaultValue="-15"
+        />
         <button
-          className="skip-forward"
+          className="btn-skip-back"
           onClick={() => {
-            skipFF(video(), ffInput());
+            skip(video(), rwInput().value);
+          }}
+        >
+          REW
+        </button>
+        <button
+          className="btn-skip-forward"
+          onClick={() => {
+            skip(video(), ffInput().value);
           }}
         >
           FF
         </button>
-        <input type="number" id="input-skip-ff" defaultValue="15" />
-        <span style={{ fontSize: '12px' }}>seconds</span>
+        <input
+          type="number"
+          id="input-skip-ff"
+          defaultValue="15"
+          min="0"
+          max="60"
+        />
       </div>
     );
   }
 }
 
-const skipFF = (media, ffVal) => {
+const skip = (media, tVal) => {
   if (media) {
     const cTime = media.currentTime;
-    const val = typeof ffVal === Number ? ffVal : Number(ffVal);
+    const val = typeof tVal === Number ? tVal : +tVal;
+    const result = cTime + val;
+    let minmax = 0 < cTime + val ? media.duration : 0;
 
     console.log('\ntime before: ' + media.currentTime.toFixed());
-    media.currentTime =
-      media.duration - val > cTime + val ? cTime + val : media.duration;
+
+    media.currentTime = minmax
+      ? minmax > result ? result : minmax
+      : result > minmax ? result : minmax;
+
     console.log('time after: ' + media.currentTime.toFixed());
   }
 };
