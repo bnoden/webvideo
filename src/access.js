b06e4a8d81c69e1ea9f0a90308fac98e2d725c2f
bnoden <brandroid369@gmail.com>
2017-10-23 23:04:07 -0500

    dry

--- src/access.js
+++ src/access.js
@@ -14,11 +14,10 @@ export const shortFileName = (file, token) => {
     let arr = file.split(/[?]/);
     arr.pop();
     filename = arr[arr.length - 1].split('/');
-    filename = filename[filename.length - 1].split('\\');
   } else {
     filename = file.split('/');
-    filename = filename[filename.length - 1].split('\\');
   }
+  filename = filename[filename.length - 1].split('\\');
   return filename[filename.length - 1];
 };
 
